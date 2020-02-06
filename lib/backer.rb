13667981c9c47385992b529524e53f8f6cb1f6b4
Micah Banschick <micha.ban@gmail.com>
2020-02-06 01:49:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,12 +3,19 @@ class Backer
   attr_accessor :backed_projects
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @backed_projects = []
+    self.save
   end
   
+  def save 
+    @@all << self 
+  end 
+  
   def back_project(project)
-    
+  end 
   
 end 
